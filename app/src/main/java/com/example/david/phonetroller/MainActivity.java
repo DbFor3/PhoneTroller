c437222fbe06f775a3d23cbfac7c98f123f66f64
David Brown <db06099@georgiasouthern.edu>
2017-03-23 17:53:12 -0400

    Accelerometer values are set up so are the buttons

--- app/src/main/java/com/example/david/phonetroller/MainActivity.java
+++ app/src/main/java/com/example/david/phonetroller/MainActivity.java
@@ -1,13 +1,202 @@
 package com.example.david.phonetroller;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    //declare the buttons for the project
+    private Button mBackButton;
+    private Button mSettingsButton;
+    private Button mStartButton;
+    private ImageButton mAButton;
+    private ImageButton mBButton;
+    private ImageButton mXButton;
+    private ImageButton mYButton;
+    private Button mLTButton;
+    private Button mRTButton;
+    private ImageButton mLeftDpadButton;
+    private ImageButton mRightDpadButton;
+    private ImageButton mUpDpadButton;
+    private ImageButton mDownDpadButton;
+
+    //get the environment sensors
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private float lastX, lastY, lastZ;
+
+    //these values we need to use to gage the user turning their phone as if it was a steering wheel
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //checking to see if device has accelerometer
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            // success! we have an accelerometer
+
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            // fail! we dont have an accelerometer!
+        }
+
+
+        //initiate the buttons and set on click listeners
+
+        mBackButton = (Button) findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mSettingsButton = (Button) findViewById(R.id.settings_button);
+        mSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mUpDpadButton = (ImageButton) findViewById(R.id.up_dpad_button);
+        mUpDpadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mDownDpadButton = (ImageButton) findViewById(R.id.down_dpad_button);
+        mDownDpadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mLeftDpadButton = (ImageButton) findViewById(R.id.left_dpad_button);
+        mLeftDpadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mRightDpadButton = (ImageButton) findViewById(R.id.right_dpad_button);
+        mRightDpadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mAButton = (ImageButton) findViewById(R.id.a_button);
+        mAButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBButton = (ImageButton) findViewById(R.id.b_button);
+        mBButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        mXButton = (ImageButton) findViewById(R.id.x_button);
+        mXButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mYButton = (ImageButton) findViewById(R.id.y_button);
+        mYButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mLTButton = (Button) findViewById(R.id.lt_button);
+        mLTButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mRTButton = (Button) findViewById(R.id.rt_button);
+        mRTButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
-}
+    //onResume() register the accelerometer for listening the events
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    //onPause() unregister the accelerometer for stop listening the events
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+
+        /**once we find out the values of x y z that simulates the user turning the phone
+            we will put the code for the left or right dpad button getting pressed here to
+            turn the in game car
+         */
+
+        // get the change of the x,y,z values of the accelerometer
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        // if the change is below 2, it is just plain noise
+        if (deltaX < 2)
+            deltaX = 0;
+        if (deltaY < 2)
+            deltaY = 0;
+        }
+    }
+
