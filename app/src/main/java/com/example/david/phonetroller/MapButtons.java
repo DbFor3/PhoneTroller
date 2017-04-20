1f1208f9cb293c352d81a849cd92d832f769e4a1
David Brown <db06099@georgiasouthern.edu>
2017-04-20 13:54:52 -0400

    updated code for mapping buttons

--- app/src/main/java/com/example/david/phonetroller/MapButtons.java
+++ app/src/main/java/com/example/david/phonetroller/MapButtons.java
@@ -1,7 +1,10 @@
 package com.example.david.phonetroller;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -68,6 +71,10 @@ public class MapButtons extends AppCompatActivity {
     private RadioButton rttoRTButton;
     private RadioButton rttoBButton;
 
+    //updateButton
+    private Button updateButton;
+
+    //wiill need to alter functionality of buttons in MainActivity
 
 
     @Override
@@ -75,7 +82,110 @@ public class MapButtons extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_button_map);
 
-        //initialize radio groups and buttons
+      //initialize radio groups
+        aGroup = (RadioGroup) findViewById(R.id.abuttonmap);
+        bGroup = (RadioGroup) findViewById(R.id.bbuttonmap);
+        xGroup = (RadioGroup) findViewById(R.id.xbuttonmap);
+        yGroup = (RadioGroup) findViewById(R.id.ybuttonmap);
+        ltGroup = (RadioGroup) findViewById(R.id.ltbuttonmap);
+        rtGroup = (RadioGroup) findViewById(R.id.rtbuttonmap);
+
+        //initialize radio buttons for each button group
+
+        //a button group
+
+        atoAButton =(RadioButton) findViewById(R.id.abutton_aradio);
+        atoBButton =(RadioButton) findViewById(R.id.bbutton_aradio);
+        atoXButton =(RadioButton)findViewById(R.id.xbutton_aradio);
+        atoYButton =(RadioButton) findViewById(R.id.ybutton_aradio);
+        atoLTButton = (RadioButton)findViewById(R.id.ltbutton_aradio);
+        atoRTButton = (RadioButton)findViewById(R.id.rtbutton_aradio);
+
+
+        //b button group
+
+        btoBButton = (RadioButton)findViewById(R.id.bbutton_bradio);
+        btoAButton = (RadioButton)findViewById(R.id.abutton_bradio);
+        btoXButton = (RadioButton) findViewById(R.id.xbutton_bradio);
+        btoYButton = (RadioButton)findViewById(R.id.ybutton_bradio);
+        btoLTButton = (RadioButton)findViewById(R.id.ltbutton_bradio);
+        btoRTButton = (RadioButton)findViewById(R.id.rtbutton_bradio);
+
+
+        //x button group
+        xtoXButton = (RadioButton)findViewById(R.id.xbutton_xradio);
+        xtoAButton = (RadioButton)findViewById(R.id.abutton_xradio);
+        xtoBButton = (RadioButton)findViewById(R.id.bbutton_xradio);
+        xtoYButton = (RadioButton)findViewById(R.id.ybutton_xradio);
+        xtoLTButton = (RadioButton)findViewById(R.id.ltbutton_xradio);
+        xtoRTButton =(RadioButton)findViewById(R.id.rtbutton_xradio);
+
+        //y button group
+
+        ytoYButton = (RadioButton)findViewById(R.id.ybutton_yradio);
+        ytoAButton = (RadioButton)findViewById(R.id.abutton_yradio);
+        ytoBButton = (RadioButton)findViewById(R.id.bbutton_yradio);
+        ytoXButton = (RadioButton)findViewById(R.id.xbutton_yradio);
+        ytoLTButton = (RadioButton)findViewById(R.id.ltbutton_yradio);
+        ytoRTButton = (RadioButton)findViewById(R.id.rtbutton_yradio);
+
+        //lt button group
+
+        lttoLTButton = (RadioButton)findViewById(R.id.ltbutton_ltradio);
+        lttoAButton = (RadioButton)findViewById(R.id.abutton_ltradio);
+        lttoBButton= (RadioButton)findViewById(R.id.bbutton_ltradio);
+        lttoXButton = (RadioButton)findViewById(R.id.xbutton_ltradio);
+        lttoYButton =(RadioButton)findViewById(R.id.ybutton_ltradio);
+        lttoRTButton = (RadioButton)findViewById(R.id.rtbutton_ltradio);
+
+
+        //rt button group
+        rttoRTButton = (RadioButton)findViewById(R.id.rtbutton_rtradio);
+        rttoAButton = (RadioButton)findViewById(R.id.abutton_rtradio);
+        rttoBButton = (RadioButton)findViewById(R.id.bbutton_rtradio);
+        rttoXButton = (RadioButton)findViewById(R.id.xbutton_rtradio);
+        rttoYButton = (RadioButton)findViewById(R.id.ybutton_rtradio);
+        rttoLTButton = (RadioButton)findViewById(R.id.ltbutton_rtradio);
+
+
+        //update button button when clicked updates checks all radio buttons
+        updateButton = (Button)findViewById(R.id.updateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            //if statements to check what is checked to map buttons correctly
+            //setting up for someone to come finish once we have functioning buttons
+
+                // agroup
+                 if(atoAButton.isChecked()){
+                 //do something
+                 } else if (atoBButton.isChecked()){
+                 //do somehting
+                 } else if (atoXButton.isChecked()){
+                 //do something
+                 } else if (atoYButton.isChecked()){
+                     //do something
+                 } else if (atoLTButton.isChecked()){
+                  //do something
+                } else if (atoRTButton.isChecked()){
+                  //do something
+             }
+                //continues like below for the rest of the buttons
+                //b group
+
+                //xgroup
+
+                //y group
+
+                //ltgroup
+
+
+                //rtgroup
+
+
+            }
+        });
 
 
     }
